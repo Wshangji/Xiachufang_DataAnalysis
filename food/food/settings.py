ef6b4efba04a981a120718e9c055f2fad9b911e5
TongWYMAC <shangdtream@gmail.com>
2022-01-24 12:11:02 +0800

    爬取菜单分类

--- food/food/settings.py
+++ food/food/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'food.spiders'
 #USER_AGENT = 'food (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- food/food/spiders/category.py
+++ food/food/spiders/category.py
@@ -4,23 +4,24 @@ from scrapy import Selector
 
 class CategorySpider(scrapy.Spider):
     name = 'category'
-    allowed_domains = ['www.xiachufang.com/category']
+    allowed_domains = ['www.xiachufang.com']
     custom_settings = {
         "USER_AGENT": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
     }
     start_urls = ['http://www.xiachufang.com/category/']
 
     def parse(self, response):
-        categorys = response.xpath("//div[@class='cates-list-all clearfix hidden']").extract()
+        categorys = response.xpath("//div[@class='cates-list-all clearfix hidden']").getall()
         for category in categorys:
             sel_category = Selector(text=category)
             # 一级菜单分类
             # category_father = sel_category.xpath("//h4/text()").extract_first().strip()
 
             # 二级菜单分类
-            items = sel_category.xpath("//ul/li/a").extract()
+            items = sel_category.xpath("//ul/li/a").getall()
             for item in items:
                 sel = Selector(text=item)
-                url = sel.xpath("//@href").extract_first()
-                name = sel.xpath("//text()").extract_first()
-                print(name+"\n"+url+"\n")
+                url = sel.xpath("//@href").get()
+                name = sel.xpath("//text()").get()
+                next_url = response.urljoin(url)
+                print(name + "\n" + next_url + "\n")
